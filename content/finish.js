4ea189bc42aa9b31bca49d323eb7a874c3cb3b54
Michael Kaply <consulting@kaply.com>
2013-09-21 13:53:33 +0000

    Unify autoconfig and extension

--- content/finish.js
+++ content/finish.js
@@ -59,58 +59,26 @@ const autoconfigPrefs = [
 'pref("general.config.obscure_value", 0);',
 ''].join("\n");
 
-
-
-const autoconfigStart = "// Autoconfig file written by CCK2\n\nvar config = ";
-const autoconfigEnd = "\n\nComponents.utils.import(\"resource://cck2/CCK2.jsm\");\nCCK2.init(config)\n";
+const autoconfigTemplate = [
+'// Autoconfig file written by CCK2',
+'',
+'var config = %config%;',
+'',
+'Components.utils.import("resource://cck2/CCK2.jsm");',
+'CCK2.init(config);',
+''].join("\n");
 
 function onFinishAutoconfig() {
-  var dir = chooseDir(window);
-  if (!dir) {
-    return;
-  }
-  var numFilesToWrite = 1 + cck2Files.length;
-  var config = getConfig();
-  var autoconfigFile = dir.clone();
-  autoconfigFile.append("cck2.cfg");
-  writeFile(autoconfigFile, autoconfigStart + JSON.stringify(config, null, 2) + autoconfigEnd, function() {
-    numFilesToWrite = numFilesToWrite - 1;
-  });
-  var cck2Dir = dir.clone();
-  cck2Dir.append("distribution");
-  if (cck2Dir.exists()) {
-    cck2Dir.remove(true);
-  }
-  cck2Dir.append("cck2");
-  cck2Dir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
-  for (var i=0; i < cck2Files.length; i++) {
-    var data = readChromeFile("chrome://cck2files/content/" + cck2Files[i]);
-    var destfile = cck2Dir.clone();
-    var splitpath = cck2Files[i].split('/');
-    for (var j=0; j < splitpath.length; j++) {
-      destfile.append(splitpath[j]);
-    }
-    destfile.create(Ci.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
-    writeFile(destfile, data, function() {
-      numFilesToWrite = numFilesToWrite - 1;
-    });
-  }
-  var resourceDir = dir.clone();
-  resourceDir.append("distribution");
-  resourceDir.append(config.id);
-  // Requerying the config wih a dir copies files
-  config = getConfig(resourceDir);
-  // PUT FILES IN THIS DIR THAT ARE REFERENCED AS NON URLS (local files)
-  // CHROME.MANIFEST POINTING TO FILES
-  // Incrememnt numFilesToWrite before writing. Decrement when written.
-  // Put in a defaults/autoconfig.js for good measure
-  // ZIP IT ALL UP  
-  // Write a zip file of distribution directory with autoconfig at the top level.
+  packageCCK2("distribution");
+}
+
+function onFinishExtension() {
+  packageCCK2("extension");
 }
 
 var zipwriter;
 
-function onFinishExtension() {
+function packageCCK2(type) {
   var type = "distribution"
   var numFilesToWrite = 0;
   var basedir = chooseDir(window);
@@ -150,7 +118,7 @@ function onFinishExtension() {
   if (type == "distribution") {
     var autoconfigFile = dir.clone();
     autoconfigFile.append("cck2.cfg");
-    writeFile(autoconfigFile, autoconfigStart + JSON.stringify(config, null, 2) + autoconfigEnd, addFileToZip(zipwriter));
+    writeFile(autoconfigFile, autoconfigTemplate.replace("%config%", JSON.stringify(config, null, 2)), addFileToZip(zipwriter));
   }
   if (type != "distribution") {
     var installRDF = installRDFTemplate.replace("%id%", config.extension.id);
