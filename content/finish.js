bca0b3c743fc3bed4860e5b05d6f82bc821cb456
Michael Kaply <consulting@kaply.com>
2013-09-21 00:03:12 +0000

    Extension building

--- content/finish.js
+++ content/finish.js
@@ -22,6 +22,38 @@ const cck2Files = [
   "modules/Utils.jsm"
 ]
 
+const installRDFTemplate = [
+'<RDF xmlns="http://www.w3.org/1999/02/22-rdf-syntax-ns#"',
+'     xmlns:em="http://www.mozilla.org/2004/em-rdf#">',
+'    <Description about="urn:mozilla:install-manifest">',
+'        <em:id>%id%</em:id>',
+'        <em:name>%name%</em:name>',
+'        <em:version>%version%</em:version>',
+'        <em:description>%description%</em:description>',
+'        <em:creator>CCK2 Wizard (http://mike.kaply.com/cck2)</em:creator>',
+'        <em:homepageURL>%homepageURL%</em:homepageURL>',
+'        <em:updateURL>%updateURL%</em:updateURL>',
+'        <em:updateKey>%updateKey%</em:updateKey>',
+'        <em:targetApplication>',
+'            <Description>',
+'                <em:id>{ec8030f7-c20a-464f-9b0e-13a3a9e97384}</em:id>',
+'                <em:minVersion>17.0</em:minVersion>',
+'                <em:maxVersion>24.*</em:maxVersion>',
+'            </Description>',
+'        </em:targetApplication>',
+'    </Description>',
+'</RDF>',
+''].join("\n");
+
+const chromeManifestTemplate = [
+'resource %id% resources/',
+'manifest cck2/chrome.manifest',
+''].join("\n");
+
+const defaultPrefsTemplate = [
+'pref("extensions.cck2.config", "%config%");',
+''].join("\n");
+
 const autoconfigStart = "// Autoconfig file written by CCK2\n\nvar config = ";
 const autoconfigEnd = "\n\nComponents.utils.import(\"resource://cck2/CCK2.jsm\");\nCCK2.init(config)\n";
 
@@ -70,7 +102,113 @@ function onFinishAutoconfig() {
 }
 
 function onFinishExtension() {
-  // merge CCK2 chrome.manifest
+  var numFilesToWrite = 0;
+  var dir = chooseDir(window);
+  if (!dir) {
+    return;
+  }
+  dir.append("xpi");
+  if (dir.exists()) {
+    dir.remove(true);
+  }
+  dir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  var config = getConfig();
+  var installRDF = installRDFTemplate.replace("%id%", config.extension.id);
+  installRDF = installRDF.replace("%name%", config.extension.name);
+  installRDF = installRDF.replace("%version%", config.extension.version);
+  if ("description" in config.extension) {
+    installRDF = installRDF.replace("%description%", config.extension.description);
+  } else {
+    installRDF = installRDF.split("\n").filter(function(element) {
+      return !/%description%/.test(element);
+    }).join("\n");
+  }
+  if ("homepageURL" in config.extension) {
+    installRDF = installRDF.replace("%homepageURL%", config.extension.homepageURL);
+  } else {
+    installRDF = installRDF.split("\n").filter(function(element) {
+      return !/%homepageURL%/.test(element);
+    }).join("\n");
+  }
+  if ("updateURL" in config.extension) {
+    installRDF = installRDF.replace("%updateURL%", config.extension.updateURL);
+  } else {
+    installRDF = installRDF.split("\n").filter(function(element) {
+      return !/%updateURL%/.test(element);
+    }).join("\n");
+  }
+  if ("updateKey" in config.extension) {
+    installRDF = installRDF.replace("%updateKey%", config.extension.updateKey);
+  } else {
+    installRDF = installRDF.split("\n").filter(function(element) {
+      return !/%updateKey%/.test(element);
+    }).join("\n");
+  }
+  var installRDFFile = dir.clone();
+  installRDFFile.append("install.rdf");
+  numFilesToWrite += 1;
+  writeFile(installRDFFile, installRDF, function() {
+    numFilesToWrite -= 1;
+  });
+  var chromeManifest = chromeManifestTemplate.replace("%id%", config.id);
+  var chromeManifestFile = dir.clone();
+  chromeManifestFile.append("chrome.manifest");
+  numFilesToWrite += 1;
+  writeFile(chromeManifestFile, chromeManifest, function() {
+    numFilesToWrite -= 1;
+  });
+  var cck2Dir = dir.clone();
+  cck2Dir.append("cck2");
+  cck2Dir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  for (var i=0; i < cck2Files.length; i++) {
+    var data = readChromeFile("chrome://cck2files/content/" + cck2Files[i]);
+    var destfile = cck2Dir.clone();
+    var splitpath = cck2Files[i].split('/');
+    for (var j=0; j < splitpath.length; j++) {
+      destfile.append(splitpath[j]);
+    }
+    destfile.create(Ci.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
+    numFilesToWrite += 1;
+    writeFile(destfile, data, function() {
+      numFilesToWrite -= 1;
+    });
+  }
+  if ("iconurl" in config.extension) {
+    var iconFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    iconFile.initWithPath(config.extension.iconurl);
+    iconFile.copyTo(dir, "icon.png");
+  }
+  var preferencesFile = dir.clone();
+  preferencesFile.append("default");
+  preferencesFile.append("preferences");
+  preferencesFile.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  preferencesFile.append("prefs.js");
+  delete config.extension;
+  var defaultPrefs = defaultPrefsTemplate.replace("%config%", JSON.stringify(config).replace(/"/g, "\\\""));
+  numFilesToWrite += 1;
+  writeFile(preferencesFile, defaultPrefs, function() {
+    numFilesToWrite -= 1;
+  });
+  if ("plugins" in config) {
+    var pluginsDir = dir.clone();
+    pluginsDir.append("plugins");
+    for (var i=0; i < config.plugins.length; i++) {
+      var pluginFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+      pluginFile.initWithPath(config.plugins[i]);
+      pluginFile.copyTo(pluginsDir, null);
+    }
+    delete (config.plugins);
+  }
+
+  var resourceDir = dir.clone();
+  resourceDir.append("resources");
+  resourceDir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  // plugins in top level plugins
+  // searchplugins in resources/searchplugins
+  // certs in resources/certs
+  // extension in resources/extensions
+  // proxy config file in resources/proxyconfig
+  
 }
 
 function readChromeFile(path) {
