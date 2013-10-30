da1b4c5db5617c517abe149582eecf65fc668b9e
mkaply <mkaply@c1ed6279-22b5-4316-8e1c-023409d10d0f>
2013-10-30 13:11:49 +0000

    True object comparison

--- trunk/cck2wizard/content/cck2wizard.js
+++ trunk/cck2wizard/content/cck2wizard.js
@@ -116,10 +116,35 @@ function onImport() {
   }
 }
 
+function objectEquals(x, y) {
+    // if both are function
+    if (x instanceof Function) {
+        if (y instanceof Function) {
+            return x.toString() === y.toString();
+        }
+        return false;
+    }
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+
+    // if one of them is date, they must had equal valueOf
+    if (x instanceof Date) { return false; }
+    if (y instanceof Date) { return false; }
+
+    // if they are not function or strictly equal, they both need to be Objects
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    var p = Object.keys(x);
+    return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) ?
+            p.every(function (i) { return objectEquals(x[i], y[i]); }) : false;
+}
+
 // Returns true if it is OK to continue, false if not
 function checkToSave() {
   var newConfig = getConfig();
-  if (gCurrentConfig && (JSON.stringify(newConfig)  != JSON.stringify(gCurrentConfig))) {
+//  if (gCurrentConfig && (JSON.stringify(newConfig)  != JSON.stringify(gCurrentConfig))) {
+  if (gCurrentConfig && (!objectEquals(newConfig, gCurrentConfig))) {
     var buttonFlags = (Services.prompt.BUTTON_POS_0) * (Services.prompt.BUTTON_TITLE_YES) +
                       (Services.prompt.BUTTON_POS_1) * (Services.prompt.BUTTON_TITLE_CANCEL) +
                       (Services.prompt.BUTTON_POS_2) * (Services.prompt.BUTTON_TITLE_NO) +
