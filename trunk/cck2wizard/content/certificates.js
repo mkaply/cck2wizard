3235235d387876cb9e2f27a5abd4a379f7a706e8
mkaply <mkaply@c1ed6279-22b5-4316-8e1c-023409d10d0f>
2013-11-14 17:53:49 +0000

    Full certs and some cleanup

--- trunk/cck2wizard/content/certificates.js
+++ trunk/cck2wizard/content/certificates.js
@@ -1,7 +1,11 @@
 var gCertificatesListbox = null;
+var gServerCertsListbox = null;
+var gCertOverridesListbox = null;
 
 function onCertificatesLoad() {
   gCertificatesListbox = document.getElementById("certificates-listbox");
+  gServerCertsListbox = document.getElementById("servercerts-listbox");
+  gCertOverridesListbox = document.getElementById("certoverrides-listbox");
 }
 window.addEventListener("load", onCertificatesLoad, false);
 
@@ -9,7 +13,20 @@ function setCertificates(config) {
   if ("certs" in config) {
     if ("ca" in config.certs) {
       for (var i=0; i < config.certs.ca.length; i++) {
-        listitem = gCertificatesListbox.appendItem(config.certs.ca[i].url, config.certs.ca[i].trust);
+        var listitem = gCertificatesListbox.appendItem(config.certs.ca[i].url, config.certs.ca[i].trust);
+        listitem.setAttribute("context", "certificate-contextmenu");
+      }
+    }
+    if ("server" in config.certs) {
+      for (var i=0; i < config.certs.server.length; i++) {
+        var listitem = gServerCertsListbox.appendItem(config.certs.server[i]);
+        listitem.setAttribute("context", "servercert-contextmenu");
+      }
+    }
+    if ("override" in config.certs) {
+      for (var i=0; i < config.certs.override.length; i++) {
+        var listitem = gCertOverridesListbox.appendItem(config.certs.override[i]);
+        listitem.setAttribute("context", "certoverride-contextmenu");
       }
     }
   }
@@ -29,6 +46,26 @@ function getCertificates(config) {
       config.certs.ca.push(cert);
     }
   }
+  if (gServerCertsListbox.itemCount > 0) {
+    if (!("certs" in config)) {
+      config.certs = {};
+    }
+    config.certs.server = [];
+    
+    for (var i=0; i < gServerCertsListbox.itemCount; i++) {
+      config.certs.server.push(gServerCertsListbox.getItemAtIndex(i).label);
+    }
+  }
+  if (gCertOverridesListbox.itemCount > 0) {
+    if (!("certs" in config)) {
+      config.certs = {};
+    }
+    config.certs.override = [];
+    
+    for (var i=0; i < gCertOverridesListbox.itemCount; i++) {
+      config.certs.override.push(gCertOverridesListbox.getItemAtIndex(i).label);
+    }
+  }
   return config;
 }
 
@@ -36,10 +73,16 @@ function resetCertificates() {
   while (gCertificatesListbox.itemCount > 0) {
     gCertificatesListbox.removeItemAt(0);
   }
+  while (gServerCertsListbox.itemCount > 0) {
+    gServerCertsListbox.removeItemAt(0);
+  }
+  while (gCertOverridesListbox.itemCount > 0) {
+    gCertOverridesListbox.removeItemAt(0);
+  }
 }
 
 function addCertificateFromURL() {
-  var retVals = { name: null, location: null};
+  var retVals = { url: null};
   window.openDialog("chrome://cck2wizard/content/url-dialog.xul", "cck2wizard-certificate", "modal,centerscreen", retVals);
   if (retVals.cancel) {
     return;
@@ -52,12 +95,50 @@ function addCertificateFromURL() {
     return;
   }
   var listitem = gCertificatesListbox.appendItem(url, "C,C,C");
+  listitem.setAttribute("context", "certificate-contextmenu");
+}
+
+function addServerCertificateFromURL() {
+  var retVals = { url: null};
+  window.openDialog("chrome://cck2wizard/content/url-dialog.xul", "cck2wizard-certificate", "modal,centerscreen", retVals);
+  if (retVals.cancel) {
+    return;
+  }
+  var url = retVals.url;
+  try {
+    Services.io.newURI(url, null, null);
+  } catch (ex) {
+    showErrorMessage("invalidurl");
+    return;
+  }
+  var listitem = gServerCertsListbox.appendItem(url);
+  listitem.setAttribute("context", "servercert-contextmenu");
+}
+
+function addCertificateOverride() {
+  var retVals = { url: null};
+  window.openDialog("chrome://cck2wizard/content/url-dialog.xul", "cck2wizard-certificate", "modal,centerscreen", retVals);
+  if (retVals.cancel) {
+    return;
+  }
+  var url = retVals.url;
+  var listitem = gCertOverridesListbox.appendItem(url);
+  listitem.setAttribute("context", "certoverride-contextmenu");
 }
 
 function addCertificateFromFile() {
-  var searchengineFile = chooseFile(window);
-  if (searchengineFile) {
-    var listitem = gCertificatesListbox.appendItem(searchengineFile.path, "C,C,C");
+  var certFile = chooseFile(window);
+  if (certFile) {
+    var listitem = gCertificatesListbox.appendItem(certFile.path, "C,C,C");
+    listitem.setAttribute("context", "certificate-contextmenu");
+  }
+}
+
+function addServerCertificateFromFile() {
+  var certFile = chooseFile(window);
+  if (certFile) {
+    var listitem = gServerCertsListbox.appendItem(certFile.path, "C,C,C");
+    listitem.setAttribute("context", "servercert-contextmenu");
   }
 }
 
@@ -68,9 +149,19 @@ function onDeleteCertificate() {
   gCertificatesListbox.removeChild(gCertificatesListbox.selectedItem);
 }
 
+function onDeleteServerCertificate() {
+  if (gServerCertsListbox.selectedIndex == -1) {
+    return;
+  }
+  gServerCertsListbox.removeChild(gServerCertsListbox.selectedItem);
+}
+
 function onKeyPressCertificate(event) {
   if (event.keyCode == event.DOM_VK_DELETE ||
       event.keyCode == event.DOM_VK_BACK_SPACE) {
-    onDeleteCertificate();
   }
+  if (event.target.selectedIndex == -1) {
+    return;
+  }
+  event.target.removeChild(event.target.selectedItem);
 }
