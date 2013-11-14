77c12070361fec8b32d3607d51e539ed6a787e99
mkaply <mkaply@c1ed6279-22b5-4316-8e1c-023409d10d0f>
2013-11-14 15:56:14 +0000

    Build script

---
+++ trunk/cck2wizard/buildit.sh
@@ -0,0 +1,15 @@
+SHORTNAME=cck2wizard
+export SHORTNAME=$SHORTNAME
+rm  $SHORTNAME*.xpi
+rm -rf $SHORTNAME
+mkdir $SHORTNAME
+cd $SHORTNAME
+rsync -r --exclude=.svn --exclude-from=../excludefile.txt ../* .
+VERSION=`grep "em:version" install.rdf | sed -e 's/[ \t]*<em:version>//;s/<\/em:version>//g'`
+export VERSION=$VERSION
+perl -pi -e 's/0.0.0/$ENV{"VERSION"}/gi' bootstrap*.js
+rm bootstrap*.js.bak
+zip -r -D ../$SHORTNAME-$VERSION.xpi *
+cd ..
+rm -rf $SHORTNAME
+#wget --post-file=$SHORTNAME-$VERSION.xpi http://localhost:8888/
