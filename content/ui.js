5d94afb961d1a9636b8a5116f6ac83b911902aef
Michael Kaply <consulting@kaply.com>
2014-02-11 20:44:05 +0000

    Allow editing selectors and handle bad selectors

--- content/ui.js
+++ content/ui.js
@@ -30,14 +30,44 @@ function resetHiddenUI() {
   }
 }
 
+function validateSelector(selector) {
+  try {
+    document.querySelector(selector);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 function onAddHiddenUI() {
-  var retVals = { name: null, location: null, folder: true};
-  window.openDialog("chrome://cck2wizard/content/bookmarks-dialog.xul", "cck2wizard-bookmark", "modal,centerscreen", retVals);
-  if (retVals.cancel) {
+  var cssSelector = {value: null};
+  var check = {value: null};
+  var result = Services.prompt.prompt(window, "CCK2", "Enter a CSS selector:", cssSelector, null, check);
+  if (!result) {
     return;
   }
-  var listitem = gHiddenUIListbox.appendItem(retVals.name);
-  listitem.setAttribute("context", "hiddenui-contextmenu")
+  if (!validateSelector(cssSelector.value)) {
+    Services.prompt.alert(window, "CCK2", "Invalid CSS Selector")
+    return;
+  }
+  var listitem = gHiddenUIListbox.appendItem(cssSelector.value);
+}
+
+function onEditHiddenUI() {
+  if (gHiddenUIListbox.selectedIndex == -1) {
+    return;
+  }
+  var cssSelector = {value: gHiddenUIListbox.selectedItem.label};
+  var check = {value: null};
+  var result = Services.prompt.prompt(window, "CCK2", "Enter a CSS selector:", cssSelector, null, check);
+  if (!result) {
+    return;
+  }
+  if (!validateSelector(cssSelector.value)) {
+    Services.prompt.alert(window, "CCK2", "Invalid CSS Selector")
+    return;
+  }
+  gHiddenUIListbox.selectedItem.label = retVals.name;
 }
 
 function onDeleteHiddenUI() {
@@ -49,7 +79,10 @@ function onDeleteHiddenUI() {
 
 
 function onKeyPressHiddenUI(event) {
-  if (event.keyCode == event.DOM_VK_DELETE ||
+  if (event.keyCode == event.DOM_VK_ENTER ||
+      event.keyCode == event.DOM_VK_RETURN) {
+    onEditHiddenUI();
+  } else if (event.keyCode == event.DOM_VK_DELETE ||
       event.keyCode == event.DOM_VK_BACK_SPACE) {
     onDeleteHiddenUI();
   }
