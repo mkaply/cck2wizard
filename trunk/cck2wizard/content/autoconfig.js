818e1c2df02158e0eb58f6c09cced9efecde314a
mkaply <mkaply@c1ed6279-22b5-4316-8e1c-023409d10d0f>
2014-05-20 14:47:06 +0000

    Forgot to check in autoconfig.js file

---
+++ trunk/cck2wizard/content/autoconfig.js
@@ -0,0 +1,7 @@
+function onChooseAutoConfigJSBefore() {
+  onChooseFile('textbox[config="AutoConfigJSBefore"]');
+}
+
+function onChooseAutoConfigJSAfter() {
+  onChooseFile('textbox[config="AutoConfigJSAfter"]');
+}
