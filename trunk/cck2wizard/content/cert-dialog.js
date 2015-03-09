ebefd151ca1d981b09a6e457a030fb91b2844e10
mkaply <mkaply@c1ed6279-22b5-4316-8e1c-023409d10d0f>
2015-03-09 15:10:09 +0000

    New cert code

--- trunk/cck2wizard/content/cert-dialog.js
+++ trunk/cck2wizard/content/cert-dialog.js
@@ -1,27 +1,56 @@
 function onLoad() {
-  document.getElementById("trustSSL").checked = true;
-  document.getElementById("trustEmail").checked = true;
-  document.getElementById("trustObjSign").checked = true;
+}
+
+function updateTrustString() {
+  trustString = "";
+  if (document.getElementById("trustSSLServer").checked) {
+    trustString += "C";
+  }
+  if (document.getElementById("trustSSLClient").checked) {
+    trustString += "T";
+  }
+  if (document.getElementById("trustSSLForce").checked) {
+    trustString += "c";
+  }
+  trustString += ",";
+  if (document.getElementById("trustEmailServer").checked) {
+    trustString += "C";
+  }
+  if (document.getElementById("trustEmailClient").checked) {
+    trustString += "T";
+  }
+  if (document.getElementById("trustEmailForce").checked) {
+    trustString += "c";
+  }
+  trustString += ",";
+  if (document.getElementById("trustObjSignServer").checked) {
+    trustString += "C";
+  }
+  if (document.getElementById("trustObjSignClient").checked) {
+    trustString += "T";
+  }
+  if (document.getElementById("trustObjSignForce").checked) {
+    trustString += "c";
+  }
+  document.getElementById("trustString").value = trustString;
+}
+
+function parseTrustString(trustString) {
+  var trust = trustString.split(",");
+  if (trustString[0].indexOf('C') != -1) {
+    document.getElementById("trustSSLServer").checked = true;
+  }
+  if (trustString[1].indexOf('C') != -1) {
+    document.getElementById("trustEmailServer").checked = true;
+  }
+  if (trustString[2].indexOf('C') != -1) {
+    document.getElementById("trustObjSignServer").checked = true;
+  }
 }
 
 function onOK() {
   var retVals = window.arguments[0];
-  retVals.certString = "";
-  if (document.getElementById("trustSSL").checked) {
-    retVals.certString += "C,";
-  } else {
-    retVals.certString += "c,";
-  }
-  if (document.getElementById("trustEmail").checked) {
-    retVals.certString += "C,";
-  } else {
-    retVals.certString += "c,";
-  }
-  if (document.getElementById("trustObjSign").checked) {
-    retVals.certString += "C";
-  } else {
-    retVals.certString += "c";
-  }
+  retVals.certString = document.getElementById("trustString").value;
 }
 
 function onCancel() {
