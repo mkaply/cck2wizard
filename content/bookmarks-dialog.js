80ee336baf8fec36dea8db093d88cb80d8d04f09
Michael Kaply <consulting@kaply.com>
2016-08-08 07:37:40 -0500

    Make sure bookmark locations are URLS - fixes #26

--- content/bookmarks-dialog.js
+++ content/bookmarks-dialog.js
@@ -1,3 +1,8 @@
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
+var gStringBundle;
+
 function onLoad() {
   var initVals = window.arguments[0];
   if (initVals.name) {
@@ -10,12 +15,22 @@ function onLoad() {
     document.getElementById('location-hbox').hidden = true;
     window.sizeToContent();
   }
+  if (initVals.stringbundle) {
+    gStringBundle = initVals.stringbundle;
+  }
 }
 
 function onOK() {
+  var url = document.getElementById('location').value;
+  try {
+    Services.io.newURI(url, null, null);
+  } catch (ex) {
+    showErrorMessage("invalidurl");
+    return false;
+  }
   var retVals = window.arguments[0];
   retVals.name = document.getElementById('name').value;
-  retVals.location = document.getElementById('location').value;
+  retVals.location = url;
 }
 
 function onCancel() {
